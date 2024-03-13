2fff49e0eff1b4a2f40a03d1cef5cf474d63298d
오상훈 <ph38230119@gmail.com>
2024-03-13 15:48:57 +0900

    [Test] TestController API 요청 테스트

--- server/src/main/java/com/inbuddy/server/test/controller/TestController.java
+++ server/src/main/java/com/inbuddy/server/test/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test")
     public ResponseEntity<?> test() {
         return ResponseEntity.ok("Test Complete");
     }
