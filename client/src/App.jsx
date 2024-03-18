1501428d6a71679ffd7d101d35f56c15db498fa3
우찬명 <chanmyeongwoo@naver.com>
2024-03-18 10:59:23 +0900

    [Test] 버튼 컴포넌트 테스트

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import { useState } from 'react';
+import { Button } from '@/components/atoms/button.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <Button onClick={() => console.log("click")}>Click me</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <Button onClick={() => console.log('click')}>Click me</Button>
+                <Button variant="error">Error</Button>
+                <Button variant="secondary">Secondary</Button>
+                <Button variant="destructive">Destructive</Button>
+            </div>
+        </>
+    );
 }
 
 export default App;
