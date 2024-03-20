8a195181e72f5f3ef445d80fbb10f729bc7eaedc
우찬명 <chanmyeongwoo@naver.com>
2024-03-20 11:19:54 +0900

    [Design] 로그인 페이지 UI 배치

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,23 +6,23 @@ import chevronsDownIcon from '@/assets/icons/chevrons-down.svg';
 
 function Login() {
     return (
-        <div className="flex flex-col mx-auto my-auto  h-[200vh]">
+        <div className="flex flex-col mx-auto my-auto h-[200vh]">
             <div className="relative h-[100vh]">
                 <img
-                    className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 inset-0 mx-auto mt-[5vh]"
+                    className="absolute w-80 top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2 z-10 mx-auto "
                     src={cloudImg}
                     alt="구름 이미지"
                 />
                 <img
-                    className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 z-20 inset-0 mx-auto mt-[5vh]"
+                    className="absolute w-80 top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2 z-20 mx-auto "
                     src={InBuddyLogoImg}
                     alt="브랜드 로고"
                 />
 
-                <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex flex-col">
-                    <img className="z-10 inset-0 mx-auto mt-[1vh]" src={naverLoginBtnImg} alt="네이버 로고 버튼" />
-                    <img className="z-10 inset-0 mx-auto mt-[1vh]" src={kakaoLoginBtnImg} alt="카카오 로고 버튼" />
-                    <img className="z-10 inset-0 mx-auto mt-[1vh]" src={chevronsDownIcon} alt="아래 스크롤 아이콘" />
+                <div className="absolute top-[80%] left-1/2 -translate-y-1/2 -translate-x-1/2 z-20 flex flex-col space-y-4 ">
+                    <img className="z-10 inset-0 mx-auto" src={naverLoginBtnImg} alt="네이버 로고 버튼" />
+                    <img className="z-10 inset-0 mx-auto" src={kakaoLoginBtnImg} alt="카카오 로고 버튼" />
+                    <img className="z-10 inset-0 mx-auto" src={chevronsDownIcon} alt="아래 스크롤 아이콘" />
                 </div>
             </div>
             <div className="h-[100vh]">아래 스크린</div>
