ca1df3f9aa984e101562cf2d774c3557ad3d8c9d
seongmin <pok_tan@naver.com>
2024-03-20 13:57:37 +0900

    [Feat] 체크인 카운터 정보 페이지 초기 구현

--- client/src/pages/CheckInCounterInfoPage.jsx
+++ client/src/pages/CheckInCounterInfoPage.jsx
@@ -1,9 +1,34 @@
+import React from "react";
+import T1 from '@/static/map/T1-3F_Test.svg'
+import Map from "@/components/atoms/Map";
+import CheckInCounterInfo from "@/components/modules/CheckInCounterInfo";
+import { P } from "@/components/atoms/p";
+import { Button } from "@/components/atoms/button";
+
+
 function CheckInCounterInfoPage() {
+
+    // 티켓 정보에 따라 달라지는 svg파일
+    const svgFile = T1;
+
+
     return (
-        <>
-            <h3>체크인 카운터 페이지</h3>
-        </>
-    );
+        <div className="w-96">
+            <P color='text-header-mainHeader' size='3xl' font='bold' className='text-center py-4 my-4'>체크인 카운터 안내</P>
+            <Map svgFile={svgFile}></Map>
+            <CheckInCounterInfo />
+            <a href="">
+                <P variant='mainHeader' size='3xl'>혹시 다른 터미널에 계신가요?</P>
+                <P>하이</P>
+                <Button variant='destructive'>asdf</Button>
+            </a>
+            <div>
+                <h1>실제 운항사란?</h1>
+                <p>공동운항 여부에 따라 어쩌꾸저쩌구</p>
+            </div>
+        </div>
+
+    )
 }
 
 export default CheckInCounterInfoPage;
