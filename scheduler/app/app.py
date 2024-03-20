6a64079d51646a85b785f8112009144c1f7136c5
오상훈 <ph38230119@gmail.com>
2024-03-21 00:08:24 +0900

    [Fix] Response Data 수정
    JSONResponse의 content에 Serializable Object를 넣어서 해결함
    이전 커밋에서는 bytes array를 넣어서 보내서 Not serializable 에러 발생했음

--- scheduler/app/app.py
+++ scheduler/app/app.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 
 from app.redis.redis import RedisManager
 from app.logger.logger import Logger
@@ -7,7 +8,6 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 logger = Logger()
-redis = RedisManager()
 
 
 @app.get("/")
@@ -17,19 +17,22 @@ async def root():
 
 
 @app.get("/api/flights/{flight_type}")
-async def flights(flight_type):
+def flights(flight_type):
+    redis = RedisManager()
     today = (datetime.datetime.today().strftime('%Y%m%d'))
     redis.select(redis.FLIGHTS)
 
-    response = None
+    data = {}
     status_code = status.HTTP_400_BAD_REQUEST
     if flight_type == 'arrive':
-        response = redis.get(today + 'A')
+        data = redis.get(today + 'A')
+        data = json.loads(data.decode() if data else '{}')
         status_code = status.HTTP_200_OK
     elif flight_type == 'departure':
-        response = redis.get(today + 'D')
+        data = redis.get(today + 'D')
+        data = json.loads(data.decode() if data else '{}')
         status_code = status.HTTP_200_OK
-    return JSONResponse(content=response, status_code=status_code)
+    return JSONResponse(content=data, status_code=status_code)
 
 
 @app.get("/hello/{name}")
