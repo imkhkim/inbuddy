e1002ea413e4562e33dda4fbdb35ca1dc4d09dbe
임서정 <sjytis14@naver.com>
2024-03-21 17:51:25 +0900

    [Feat] KakaoOAuth2UserInfo 추가

---
+++ server/src/main/java/com/inbuddy/server/user/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,22 @@
+package com.inbuddy.server.user.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+
+    }
+
+}
---
+++ server/src/main/java/com/inbuddy/server/user/info/OAuth2UserPrincipal.java
@@ -0,0 +1,64 @@
+package com.inbuddy.server.user.info;
+
+import java.util.Collection;
+import java.util.Map;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+
+@Getter
+public class OAuth2UserPrincipal implements OAuth2User, UserDetails {
+
+    private final OAuth2UserInfo userInfo;
+
+    public OAuth2UserPrincipal(OAuth2UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
