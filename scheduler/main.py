6c1197722e6bd35d901eb334c0b2dbba82da7281
오상훈 <ph38230119@gmail.com>
2024-03-25 17:40:52 +0900

    [Add] KafkaProducer class 생성
    Entrypoint에서 Kafka 메시지 브로커에 메시지를 전송할 Producer를 정의합니다.
    실시간 날씨, 실시간 항공편을 매 분 전송하고, 하루 동안의 날씨, 하루 동안의 항공편을 하루에 한 번 전송합니다.

--- scheduler/main.py
+++ scheduler/main.py
@@ -10,10 +10,9 @@ from app.scheduler.flight_data_fetcher import fetch as flight_fetch
 from app.scheduler.weather_data_fetcher import fetch as weather_fetch
 from app.scheduler.batch_save import save
 from app.scheduler import weather_data_fetcher
-
+from app.producer.producer import live_weather_producer, live_flight_producer, \
+    batch_flight_producer, batch_weather_producer
 from app.app import app
-
-global REDIS_HOST
 
 if __name__ == "__main__":
     load_dotenv()
@@ -24,8 +23,27 @@ if __name__ == "__main__":
 
     redis.set_connection(REDIS_HOST, REDIS_PORT)
 
+    KAFKA_HOST = os.getenv("KAFKA_HOST")
+    KAFKA_FLIGHT_BROKER_PORT = os.getenv("KAFKA_FLIGHT_BROKER_PORT")
+    KAFKA_WEATHER_BROKER_PORT = os.getenv("KAFKA_WEATHER_BROKER_PORT")
+
+    live_flight_producer.set_producer(
+            server=f"{KAFKA_HOST}:{KAFKA_FLIGHT_BROKER_PORT}",
+            client_id="live_flight")
+    live_weather_producer.set_producer(
+            server=f"{KAFKA_HOST}:{KAFKA_WEATHER_BROKER_PORT}",
+            client_id="live_weather")
+    batch_flight_producer.set_producer(
+            server=f"{KAFKA_HOST}:{KAFKA_FLIGHT_BROKER_PORT}",
+            client_id="batch_weather")
+    batch_weather_producer.set_producer(
+            server=f"{KAFKA_HOST}:{KAFKA_WEATHER_BROKER_PORT}",
+            client_id="batch_weather")
+
+    log.info("Starting scheduler...")
     scheduler.create("flights_departure", flight_fetch, trigger="cron",
                      minute='*')
+
     scheduler.create("weather", weather_fetch, trigger="cron", minute='*')
 
     scheduler.create("batch_save", save, trigger="cron", hour=0, minute=0)
@@ -33,6 +51,8 @@ if __name__ == "__main__":
     scheduler.start("flights_departure")
     scheduler.start("weather")
     scheduler.start("batch_save")
+
+    log.info("Scheduler started.")
 
     uvicorn_log_config = uvicorn.config.LOGGING_CONFIG
     uvicorn_log_config["formatters"]["default"]["fmt"] \
@@ -43,4 +63,4 @@ if __name__ == "__main__":
         = uvicorn_log_config["formatters"]["default"]["datefmt"] \
         = log.date_format
 
-    uvicorn.run(app, reload=True)
+    uvicorn.run(app)
