ab8aac9332857271003c88c48c041f5006fe714d
우찬명 <chanmyeongwoo@naver.com>
2024-03-29 08:50:41 +0900

    [Feat] 로그인 코드 일부 반영

--- client/src/router/main.jsx
+++ client/src/router/main.jsx
@@ -1,4 +1,8 @@
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import store from '@/stores/store.js';
+import { Provider } from 'react-redux';
+import { useState } from 'react';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -20,72 +24,85 @@ import PassportInfoPage from '@/pages/PassportInfoPage';
 import RoamingInfoPage from '@/pages/RoamingInfoPage';
 import TerminalTransportInfoPage from '@/pages/TerminalTransportInfoPage';
 
-const isLoggedIn = false; // 로그인 여부에 따라 조건 설정
+const queryClient = new QueryClient();
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: isLoggedIn ? <Navigate to="/login" /> : <Navigate to="/main" />,
-    },
-    {
-        path: '/test',
-        element: <Test />,
-    },
-    {
-        path: '/login',
-        element: <Login />,
-    },
-    {
-        path: '/',
-        element: <BottomNav />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: '/main',
-                element: <JourneyCollectionPage />,
-            },
-            {
-                path: '/checklist',
-                element: <CheckListPage />,
-            },
-            {
-                path: '/checkincounterinfo',
-                element: <CheckInCounterInfoPage />,
-            },
-            {
-                path: '/flightticketinfo',
-                element: <FlightTicketInfoPage />,
-            },
-            {
-                path: '/settings',
-                element: <SettingsPage />,
-            },
-            {
-                path: '/info/exchange',
-                element: <ExchangeInfoPage />,
-            },
-            {
-                path: '/info/passport',
-                element: <PassportInfoPage />,
-            },
-            {
-                path: '/info/roaming',
-                element: <RoamingInfoPage />,
-            },
-            {
-                path: '/info/baggage',
-                element: <BaggageInfoPage />,
-            },
-            {
-                path: '/info/terminaltransport',
-                element: <TerminalTransportInfoPage />,
-            },
-        ],
-    },
-]);
+const Main = () => {
+    const [loggedIn, setLoggedIn] = useState(true);
+    const [email, setEmail] = useState('');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: loggedIn ? <Navigate to="/main" /> : <Navigate to="/login" />,
+        },
+        {
+            path: '/test',
+            element: <Test />,
+        },
+        {
+            path: '/login',
+            element: <Login setLoggedIn={setLoggedIn} setEmail={setEmail} />,
+        },
+        {
+            path: '/',
+            element: <BottomNav />,
+            errorElement: <ErrorPage />,
+            children: [
+                {
+                    path: '/main',
+                    element: <JourneyCollectionPage email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />,
+                },
+                {
+                    path: '/checklist',
+                    element: <CheckListPage />,
+                },
+                {
+                    path: '/checkincounterinfo',
+                    element: <CheckInCounterInfoPage />,
+                },
+                {
+                    path: '/flightticketinfo',
+                    element: <FlightTicketInfoPage />,
+                },
+                {
+                    path: '/settings',
+                    element: <SettingsPage />,
+                },
+                {
+                    path: '/info/exchange',
+                    element: <ExchangeInfoPage />,
+                },
+                {
+                    path: '/info/passport',
+                    element: <PassportInfoPage />,
+                },
+                {
+                    path: '/info/roaming',
+                    element: <RoamingInfoPage />,
+                },
+                {
+                    path: '/info/baggage',
+                    element: <BaggageInfoPage />,
+                },
+                {
+                    path: '/info/terminaltransport',
+                    element: <TerminalTransportInfoPage />,
+                },
+            ],
+        },
+    ]);
+
+    return (
+        <React.StrictMode>
+            <Provider store={store}>
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={router} />
+                </QueryClientProvider>
+            </Provider>
+        </React.StrictMode>
+    );
+};
+
+export default Main;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
