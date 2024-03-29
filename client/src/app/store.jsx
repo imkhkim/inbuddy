84c777344d2598a4bf9793f88252be78b2693f55
seongmin <pok_tan@naver.com>
2024-03-29 11:26:08 +0900

    [Fix] store 병합되지 않던 문제 수정

--- client/src/app/store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import journeyReducer from './journeySlice';
-export const store = configureStore({
-    reducer: {
-        journey: journeyReducer,
-    },
-})
