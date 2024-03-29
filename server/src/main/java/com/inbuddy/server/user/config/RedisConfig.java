10c06ea6abb76b14ffbdd0eff77ea9e4445e19e8
임서정 <sjytis14@naver.com>
2024-03-29 09:34:34 +0900

    [Feat] 로그인한 user의 refresh token, 로그아웃한 user의 access token 저장하는 redis database 5번으로 변경

--- server/src/main/java/com/inbuddy/server/user/config/RedisConfig.java
+++ server/src/main/java/com/inbuddy/server/user/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
@@ -15,9 +16,19 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.database}")
+    private int database;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        // RedisStandaloneConfiguration 객체를 사용하여 호스트, 포트, 데이터베이스 번호 설정
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setDatabase(database); // 데이터베이스 번호 설정
+
+        // RedisStandaloneConfiguration을 사용하여 LettuceConnectionFactory 생성
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
 }
