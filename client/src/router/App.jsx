665f11faca44e10c0deba4be30e8174452174541
우찬명 <chanmyeongwoo@naver.com>
2024-04-04 02:31:34 +0900

    Revert "Merge branch 'feat/fe/connect-apis-v1' into 'dev/fe'"
    This reverts merge request !108

--- client/src/router/App.jsx
+++ client/src/router/App.jsx
@@ -43,7 +43,7 @@ const App = () => {
                     element: <JourneyCollectionPage />,
                 },
                 {
-                    path: '/checklist/:journeyId',
+                    path: '/checklist',
                     element: <CheckListPage />,
                 },
                 {
