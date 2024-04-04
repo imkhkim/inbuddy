d377cfb9bc8c20832a0d1ae5fa8f3c7c1598ed48
오상훈 <ph38230119@gmail.com>
2024-04-04 11:01:39 +0900

    [Feat] 항공편 정보 API

--- scheduler/app/app.py
+++ scheduler/app/app.py
@@ -4,8 +4,10 @@ import json
 from redis_manager.redis import redis
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from app.models import PredictRequestEntity, PredictResponseEntity
+from app.models import PredictRequestEntity, PredictResponseEntity, \
+    APIResponseEntity
 from app.predict import predict
+from config import resource_lock
 
 app = FastAPI()
 
@@ -16,22 +18,20 @@ async def root():
                         status_code=status.HTTP_200_OK)
 
 
-@app.get("/api/flights/{flight_type}")
-def flights(flight_type):
+@app.get("/api/flights/{flight_code}")
+def flights(flight_code):
     today = (datetime.datetime.today().strftime('%Y%m%d'))
-    redis.select(redis.FLIGHTS_API)
 
-    data = {}
-    status_code = status.HTTP_400_BAD_REQUEST
-    if flight_type == 'arrive':
-        data = redis.get(today + 'A')
-        data = json.loads(data.decode() if data else '{}')
-        status_code = status.HTTP_200_OK
-    elif flight_type == 'departure':
-        data = redis.get(today + 'D')
-        data = json.loads(data.decode() if data else '{}')
-        status_code = status.HTTP_200_OK
-    return JSONResponse(content=data, status_code=status_code)
+    with resource_lock:
+        redis.select(redis.FLIGHTS_API)
+        document = json.loads(redis.get(today + 'D'))
+
+    http_status, message, data = 400, "불러오기 실패", None
+
+    if flight_code and flight_code in document:
+        http_status, message, data = 200, "불러오기 성공", document[flight_code]
+
+    return APIResponseEntity(status=http_status, message=message, data=data)
 
 
 @app.post("/api/predict")
